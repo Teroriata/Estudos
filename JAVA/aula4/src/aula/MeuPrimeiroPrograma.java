4b1faaf75ea1842451010bc645c7f8ffacd39913
Mateus Silva de Sousa <mateus_sousa@discente.ufg.br>
2025-04-22 09:01:38 -0300

    java

---
+++ JAVA/aula4/src/aula/MeuPrimeiroPrograma.java
@@ -0,0 +1,11 @@
+package aula;
+
+public class MeuPrimeiroPrograma {
+	
+	public static void main(String[] args) {
+		System.out.println("Ola mundo, eu so o primeiro programa em java\n");
+		System.out.println(" 2 + 6n");
+
+	}
+	
+}
